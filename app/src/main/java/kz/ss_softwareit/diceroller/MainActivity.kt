370dc9c02e432ca9621b94b9e16d08e3a9537f4d
Sultan Suleimenov <ss.softwareit@gmail.com>
2022-09-21 00:47:23 +0600

    added 2nd textview

--- app/src/main/java/kz/ss_softwareit/diceroller/MainActivity.kt
+++ app/src/main/java/kz/ss_softwareit/diceroller/MainActivity.kt
@@ -1,11 +1,34 @@
 package kz.ss_softwareit.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = arrayOf(dice.roll(), dice.roll())
+        val resultTextView1: TextView = findViewById(R.id.textView1)
+        resultTextView1.text = diceRoll[0].toString()
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = diceRoll[1].toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
