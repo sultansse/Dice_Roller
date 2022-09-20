da2e687571137057944cb9665728dc7b50c31d02
Sultan Suleimenov <ss.softwareit@gmail.com>
2022-09-20 23:32:08 +0600

    Initial commit

---
+++ app/src/main/java/kz/ss_softwareit/diceroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package kz.ss_softwareit.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
